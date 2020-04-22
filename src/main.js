42d120df65dcb5321db788c63cd1e8ae13f99c32
brevin <cronkbrevin98@gmail.com>
2020-04-22 14:46:26 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+// import { Keys } from '../keys.js';
+// require('dotenv').config();
 
 $(document).ready(function() {
+	let url = 'https://api.covid19api.com/summary';
 	$('#searchButton').click(function(event) {
 		event.preventDefault();
-		console.log('works');
+
+		let request = new XMLHttpRequest();
+
+		request.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				const response = JSON.parse(this.responseText);
+				getElements(response);
+			}
+		};
+
+		request.open(`GET`, url, true);
+		request.send();
+
+		const getElements = function(response) {
+			console.log(response);
+			console.log(response.Countries);
+			response.Countries.forEach(function(country) {
+				$('#display-country').append(`Country: ${country.Country}`);
+			});
+		};
 	});
 });
