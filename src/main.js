e24d870c3ff96b71b4f7fc730d782e492b12c30f
brevin <cronkbrevin98@gmail.com>
2020-04-22 15:49:32 -0700

    work with API and display

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import $ from 'jquery';
 
 $(document).ready(function() {
 	let url = 'https://api.covid19api.com/summary';
+
 	$('#searchButton').click(function(event) {
 		event.preventDefault();
+		$('#searchButton').hide();
 
 		let request = new XMLHttpRequest();
 
@@ -23,11 +25,31 @@ $(document).ready(function() {
 		request.send();
 
 		const getElements = function(response) {
-			console.log(response);
-			console.log(response.Countries);
+			// console.log(response);
+			// console.log(response.Countries);
+			$('#search-thing').append('<select class="form-style" id="country" name="country">');
 			response.Countries.forEach(function(country) {
-				$('#display-country').append(`Country: ${country.Country}`);
+				// console.log(country.Country);
+				// console.log(country.Slug);
+				$('#country').append('<option value="' + country.Slug + '">' + country.Country + '</option>');
+				// $('#display-country').append(
+				// 	`Country: ${country.Country}` +
+				// 		'<br>' +
+				// 		'<br>' +
+				// 		'<li>' +
+				// 		`Total Cases: ${country.TotalConfirmed}` +
+				// 		'<br>' +
+				// 		'<li>' +
+				// 		`Total Deaths: ${country.TotalDeaths}` +
+				// 		'<br>' +
+				// 		'<li>' +
+				// 		`Total Recovered: ${country.TotalRecovered}` +
+				// 		'<br>' +
+				// 		'<br>'
+				// );
 			});
+			$('#search-thing').append('</select>');
+			$('country);
 		};
 	});
 });
